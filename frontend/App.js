e2a99fee37dc16d58c8fc16f19b5a9b86911439b
Rebecca Fendt <rebecca.fen@posteo.de>
2023-02-03 15:25:58 +0100

    Changed Chat to GiftedChat, because sending and displaying messages is easier. Readme needs to be filled with instructions

---
+++ frontend/App.js
@@ -0,0 +1,23 @@
+import {StatusBar} from 'expo-status-bar';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import {Component} from "react";
+import Chat from "./components/Chat";
+
+function App() {
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Chat/>
+            <StatusBar style="auto"/>
+        </SafeAreaView>
+    );
+
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
---
+++ frontend/README.md
@@ -0,0 +1,11 @@
+# Get GGEEZ Client up and running
+
+## Description
+
+GGEEZ is a tool to help you choose a video game. 
+
+## Installations
+
+## Usage
+
+## Contributing
